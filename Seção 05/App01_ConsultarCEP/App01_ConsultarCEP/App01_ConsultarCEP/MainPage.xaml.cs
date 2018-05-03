c16f0a2632a6220d5db9f6b58b118f8784bef108
Anderson Silva <ander.loirao@gmail.com>
2018-05-03 00:26:58 -0300

    inclusao

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Servico;
+using App01_ConsultarCEP.Servico.Modelo;
+
+
+
+namespace App01_ConsultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;  // += contatenar e atribuir
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            // TODO - lógica do programa
+
+            // TODO - VALIDAÇOES
+            string cep = CEP.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = string.Format("Endereço: {2}, {3}, {0}, {1}.", end.localidade, end.uf, end.logradouro, end.bairro);
+
+
+        }
+		
+	}
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo; // deserializar
+using Newtonsoft.Json;
+
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        // busca endereco do site viacep.com.br
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(String cep)
+        {
+            string NovoEnderecoURL = String.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            String Conteudo = wc.DownloadString(NovoEnderecoURL); // metodo sincrono -- trava tela
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo); // converte o Json recebido da net em objeto
+
+            return end;
+
+        }
+
+    }
+}
